9c9d7c9d5269a021f927c59c7f8a78da7508f8c0
Ando <108226527+andododo@users.noreply.github.com>
2024-07-20 07:19:49 +0800

    Update db_connection.py
    removed credentials

--- db_connection.py
+++ db_connection.py
@@ -3,9 +3,9 @@ import pyodbc
 def connect():
     # Connect to SQL Server
     conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};'
-                        'SERVER=ATPWEBDBN1;'
-                        'DATABASE=dbWorkFlow;'
-                        'UID=wfWI;'
-                        'PWD=ATPWI2018!')
+                        'SERVER=;'
+                        'DATABASE=;'
+                        'UID=;'
+                        'PWD=')
     
     return conn
